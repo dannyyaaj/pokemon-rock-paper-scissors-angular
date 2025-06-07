7405b655060a830775445cdd429f8a74e0a454f4
Danny Yang <dannyyaaj@gmail.com>
2025-06-07 18:06:37 -0500

    add mock API response to and validate behavior of pokemon service unit test

--- src/app/services/pokemon.service.spec.ts
+++ src/app/services/pokemon.service.spec.ts
@@ -1,10 +1,22 @@
 import { TestBed } from '@angular/core/testing'
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { PokemonService } from './pokemon.service'
+import { Pokemon } from '../models/pokemon'
 
 describe('PokemonService', () => {
   let service: PokemonService
   let httpMock: HttpTestingController
+
+  const mockPokemon: Pokemon = {
+    name: 'bulbasaur',
+    id: 1,
+    sprites: {
+      front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+    },
+    order: 1,
+    height: 7,
+    weight: 69
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -21,5 +33,15 @@ describe('PokemonService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should fetch Pokemon details by name', () => {
+    service.getPokemonDetail('bulbasaur').subscribe(pokemon => {
+      expect(pokemon.name).toBe('bulbasaur')
+    })
+
+    const request = httpMock.expectOne('https://pokeapi.co/api/v2/pokemon/bulbasaur')
+    expect(request.request.method).toBe('GET')
+    request.flush(mockPokemon)
   })
 })
